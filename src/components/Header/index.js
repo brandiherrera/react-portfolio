299f5e0b9f87b1a10d20e8cf9ab32633494e198d
brandiherrera <bcjherrera@gmail.com>
2021-02-28 14:10:49 -0800

    update header, add header pic

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,14 @@
 import './index.css';
+import PortfolioImg from '../../images/portfolio-pic.jpg';
 
 function Header() {
   return (
-    <div className='column-flex-container section'>
-      <h1>This is my header</h1>
-      <h2>This is my subheader</h2>
-      <div className='my-pic'></div>
+    <div className='header column-flex-container section'>
+      <h1>Full-Stack Developer, UX Enthusiast & Team Leader</h1>
+      <h2 className='content-section' style={{ color: '#737373' }}>
+        Hi, I'm Brandi and I like to create beautiful user experiences
+      </h2>
+      <img className='my-pic' src={ PortfolioImg } alt='portfolio header' />
     </div>
   )
 }
