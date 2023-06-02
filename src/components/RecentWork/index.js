624f7341cf9ea40980d0460199cfb9448be60940
brandiherrera <bcjherrera@gmail.com>
2023-06-02 13:44:09 -0400

    update work

--- src/components/RecentWork/index.js
+++ src/components/RecentWork/index.js
@@ -5,27 +5,18 @@ import { work } from '../../utils/workUtil';
 
 function RecentWork() {
   return (
-    <div className='column-flex-container section'>
-      <h2 style={{ color: '#FFF' }}>Recent Work</h2>
-      <div className='work-tile-container'>
-        {
-          work.map((w, i)=> (
-            <div className='container' key={ i }>
-              <img src={ w.preview } alt={ w.title } className='image' />
-              <div className='overlay'>
-                <WorkTile
-                  key={ i }
-                  title={ w.title }
-                  desc={ w.desc }
-                  codeLink={ w.codeLink }
-                  liveLink={ w.liveLink }
-                  techStack={ w.techStack }
-                />
-              </div>
-            </div>
-          ))
-        }
-      </div>
+    <div className='work-tile-container'>
+      {
+        work.map((w, i)=> (
+          <WorkTile
+            key={ i }
+            company={ w.company }
+            image={ w.image }
+            link={ w.link }
+            role={ w.role }
+          />
+        ))
+      }
     </div>
   )
 }
--- src/components/WorkTile/index.js
+++ src/components/WorkTile/index.js
@@ -1,24 +1,20 @@
 import './index.css';
 
-function WorkTile({ title, desc, codeLink, liveLink, techStack }) {
+function WorkTile({ company, image, link, role }) {
   return (
-    <div className='work-tile column-flex-container'>
-      <h4>{ title }</h4>
-      <p>{ desc }</p>
-      <div className='flex-container'>
-        <a href={ codeLink } rel='noopener noreferrer' target='_blank'>
-          <button className='work-tile-button'>
-            Visit Code
-          </button>
-        </a>
-        <a href={ liveLink } rel='noopener noreferrer' target='_blank'>
-          <button className='work-tile-button'>
-            Visit Website
-          </button>
-        </a>
-      </div>
-      <p className='tech-icons'>{ techStack }</p>
-    </div>
+    <a href={ link } rel='noopener noreferrer' target='_blank'>
+      <button className='work-tile'>
+        <img src={ image } alt={ company } className='image' />
+        <div className='flex-container-spaced'>
+          <p style={{ position: 'absolute', bottom: 0, left: 0, marginLeft: '24px' }}>
+            { role }
+          </p>
+          <p style={{ position: 'absolute', bottom: 0, right: 0, marginRight: '24px', color: '#ccc' }}>
+            { company }
+          </p>
+        </div>
+      </button>
+    </a>
   )
 }
 
