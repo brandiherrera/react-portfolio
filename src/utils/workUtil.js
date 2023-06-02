e7affbe6cc2a1b1b3c9099d833b2b719473d3afc
brandiherrera <bcjherrera@gmail.com>
2023-06-02 13:43:39 -0400

    update work

--- src/utils/workUtil.js
+++ src/utils/workUtil.js
@@ -1,73 +1,24 @@
-import {
-  IoLogoHtml5,
-  IoLogoCss3,
-  IoLogoJavascript,
-  IoLogoNodejs,
-} from 'react-icons/io';
-import { SiJquery, SiPostgresql, SiReact } from 'react-icons/si';
-
-import MuseMe from '../images/museme.png'
-import MyBrainTracker from '../images/my-brain-tracker.png'
-import Noteful from '../images/noteful.png'
-import Travodex from '../images/travodex.png'
+import luna from '../images/luna.png'
+import thinkful from '../images/thinkful.png'
+import kjwf from '../images/kjwf.png'
 
 export const work = [
   {
-    title: 'MuseMe',
-    preview: MuseMe,
-    desc: "Let's explore art together.",
-    codeLink: 'https://github.com/brandiherrera/museum-app-full-stack-capstone-react',
-    liveLink: 'https://museum-app-full-stack-capstone-react.brandiherrera.now.sh/',
-    techStack: [
-      <IoLogoHtml5 />,
-      <IoLogoCss3 />,
-      <IoLogoJavascript />,
-      <SiReact />,
-      <IoLogoNodejs />,
-      <SiPostgresql />,
-    ],
+    company: 'Luna',
+    image: luna,
+    link: 'https://www.lunadna.com',
+    role: 'Software Engineer'
   },
   {
-    title: 'My-Brain Tracker',
-    preview: MyBrainTracker,
-    desc: 'Understand your migraine triggers and symptoms.',
-    codeLink: 'https://github.com/brandiherrera/migraine-app-full-stack-capstone-react',
-    liveLink: 'https://migraine-app-full-stack-capstone-react.brandiherrera.now.sh/',
-    techStack: [
-      <IoLogoHtml5 />,
-      <IoLogoCss3 />,
-      <IoLogoJavascript />,
-      <SiReact />,
-      <IoLogoNodejs />,
-      <SiPostgresql />,
-    ],
+    company: 'KJWF',
+    image: kjwf,
+    link: 'https://www.kenjohnstonwarriorfund.com',
+    role: 'Designer/Developer'
   },
   {
-    title: 'Travodex',
-    preview: Travodex,
-    desc: 'Explore the world from your couch.',
-    codeLink: 'https://github.com/brandiherrera/travel-destination-search-api-capstone',
-    liveLink: 'https://brandiherrera.github.io/travel-destination-search-api-capstone/',
-    techStack: [
-      <IoLogoHtml5 />,
-      <IoLogoCss3 />,
-      <SiJquery />,
-      <IoLogoJavascript />,
-    ],
-  },
-  {
-    title: 'Noteful',
-    preview: Noteful,
-    desc: 'Organize your to-do list once and for all.',
-    codeLink: 'https://github.com/brandiherrera/noteful-app',
-    liveLink: 'https://noteful-app.brandiherrera.now.sh/',
-    techStack: [
-      <IoLogoHtml5 />,
-      <IoLogoCss3 />,
-      <IoLogoJavascript />,
-      <SiReact />,
-      <IoLogoNodejs />,
-      <SiPostgresql />,
-    ],
+    company: 'Thinkful',
+    image: thinkful,
+    link: 'https://www.thinkful.com',
+    role: 'Fullstack Apprentice'
   },
 ]
