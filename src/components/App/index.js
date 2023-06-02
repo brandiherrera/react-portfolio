d26ebf7d32a8d5fce2cd4a0d492a8a05ecca89fa
brandiherrera <bcjherrera@gmail.com>
2023-06-02 13:43:57 -0400

    update app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,33 +1,21 @@
-import About from '../About'
 import Footer from '../Footer'
 import Header from '../Header'
 import Nav from '../Nav'
 import RecentWork from '../RecentWork'
-import Skills from '../Skills'
 
 function App() {
   return (
     <div>
-      <div className='diagonal-box'>
-        <div className='content'>
-          <nav>
-            <Nav />
-          </nav>
-          <Header />
-        </div>
-      </div>
+      <nav>
+        <Nav />
+      </nav>
+      <Header />
       <main>
-        <About />
-        <div className='diagonal-box'>
-          <div className='content'>
-            <Skills />
-          </div>
-        </div>
         <RecentWork />
       </main>
-        <footer>
-          <Footer />
-        </footer>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   )
 }
